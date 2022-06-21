e56865c5eb4e35b9ceda9f17e2d5ff843e846fd2
alexparapap <alex.pap94@hotmail.gr>
2022-06-22 00:35:40 +0300

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,6 +18,7 @@ namespace API
 {
     public class Startup
     {
+        private const string Origins = "https:/localhost:4200";
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
@@ -33,10 +34,8 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
+            services.AddCors();
+   
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,14 +44,13 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
